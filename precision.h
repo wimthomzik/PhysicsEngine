4825cf5eadc22428e4e924686c7231e1b652ae87
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2025-04-08 16:14:20 +0200

    Update precision.h
    Define Precision of the sqrt operator; add header guard

--- precision.h
+++ precision.h
@@ -1,5 +1,13 @@
+#ifndef PRECISION_H
+#define PRECISION_H
+
 namespace wengine {
   // Defines a real number precision - default single precision
   // Reason: Change precision of the whole engine by changing typedef
   typedef float real;
+
+  // Defines the precision of the square root operator
+  #define real_sqrt sqrtf
 }
+
+#endif // PRECISION_H
